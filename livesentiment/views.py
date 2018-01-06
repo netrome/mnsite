6725ee45f25cb9d405ce302fa7a2d6c6ad2f689e
Marten <marten3@kth.se>
2018-01-06 21:06:47 +0100

    Live sentiment now connects to external host.

--- livesentiment/views.py
+++ livesentiment/views.py
@@ -25,7 +25,7 @@ def get_value(request):
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("localhost", 8095))
+        s.connect(("192.168.1.101", 8095))
         s.send(text.encode("utf-8"))
         rec = s.recv(1024).decode("utf-8")
     except Exception:
